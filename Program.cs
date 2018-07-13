3bee13f6e9c861add00c111f30313f462f97f0be
wilson-pc <wilverhidalgo@outlook.com>
2018-07-13 11:14:39 -0400

    persona

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace pruebasGit
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("soy .netcore 2.1");
+        }
+
+        public static List<Persona> Data(){
+            List<Persona> Lista;
+             Lista= new List<Persona>{
+                      new Persona{Nombre="Wilson",Apellido="PC"},
+                       new Persona {Nombre="GALLO",Apellido="gALLINA"}
+             };
+         return new List<Persona>();
         }
     }
 }
