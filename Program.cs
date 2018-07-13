4f3160b1a2dbc47b062a56c0176337b186da9db1
Angel <acherfun99@gmail.com>
2018-07-13 11:09:11 -0400

    Sumas

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace pruebasGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("soy .netcore 2.1");
+            
+            var suma=1;
+            for(int i=0;i<10;i++){
+                Console.WriteLine(suma+"+"+i+"="+(suma+i) );
+            }
+            
         }
     }
 }
